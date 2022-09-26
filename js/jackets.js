c87860232d675a3232a3d898e3464aef72cb1759
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2022-09-26 18:10:28 +0200

    new

--- js/jackets.js
+++ js/jackets.js
@@ -1,15 +1,40 @@
 const jacketsConatiner = document.querySelector(".jackets");
+const categoriBtn = document.querySelectorAll(".btn-category");
 
-for (let i = 0; i < products.length; i++) {
-  jacketsConatiner.innerHTML += `<a href="jacket-specific.html?id=${products[i].id}">
-                                  <figure class="jacket">
-                                   <img class="product-img" src="${products[i].image}" alt="${products[i].name}"/>
-                                  <figcaption class="jacket-text">
-                                    <h3 class="heading-tertiary">${products[i].name}</h3>
-                                    <p class="jacket-nr">${products[i].product_code}</p>
-                                    <span class="product-rating">${products[i].rating} (${products[i].id}) </span>
-                                    <p class="price">${products[i].price} kr</p>
-                                  </figcaption>
-                                  </figure>
-                                </a> `;
+for (let i = 0; i < categoriBtn.length; i++) {
+  categoriBtn[i].addEventListener("click", filterProducts.bind(this, categoriBtn[i]));
+}
+
+function filterProducts(item) {
+  changeAvtiveStyle(item);
+  console.log(item.attributes.id.value);
+
+  const filterdProduct = products.filter(({ category }) => category == item.attributes.id.value);
+  console.log(filterdProduct);
+
+  displayProduct(filterdProduct);
+}
+
+function changeAvtiveStyle(activeItem) {
+  for (let i = 0; i < categoriBtn.length; i++) {
+    categoriBtn[i].classList.remove("active");
+  }
+  activeItem.classList.add("active");
+}
+
+function displayProduct(products) {
+  jacketsConatiner.innerHTML = "";
+  for (let i = 0; i < products.length; i++) {
+    jacketsConatiner.innerHTML += `<a href="jacket-specific.html?id=${products[i].id}" >
+                                    <figure class="jacket">
+                                      <img class="product-img" src="${products[i].image}" alt="${products[i].name}"/>
+                                    <figcaption class="jacket-text">
+                                      <h3 class="heading-tertiary">${products[i].name}</h3>
+                                      <p class="jacket-nr">${products[i].product_code}</p>
+                                      <span class="product-rating">${products[i].rating} (${products[i].id}) </span>
+                                      <p class="price">${products[i].price} kr</p>
+                                    </figcaption>
+                                    </figure>
+                                  </a> `;
+  }
 }
